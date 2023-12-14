32ea607476ed7f8a5e38642d1b781a256645cda4
ilham <ibouaik@gmail.com>
2023-12-14 12:28:13 +0100

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :mettings
   devise_for :users
+  resources :users, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
