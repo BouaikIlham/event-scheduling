0c3d0137090a747bfa2c876c25eb7c2919412819
ilham <ibouaik@gmail.com>
2023-12-19 21:07:33 +0100

    sign out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :mettings
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :users, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
